e7abe64dc43b6a374f171f398c1a4742818dbd98
MithunM1998 <68276729+MithunM1998@users.noreply.github.com>
2023-03-09 17:48:40 +0530

    Add files via upload

---
+++ IfELseProgram.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class IfELseProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age=10;
+		if(age<=18) {
+			System.out.println("Give ballon");
+		}
+		else 
+		{
+			System.out.println("Allow inside only");
+		}
+	}
+
+}
